3bac69b774efc97f1144988683aba4433c7942d7
Revathi <reva.avula@gmail.com>
2020-11-03 16:06:29 -0600

    Finished Exercise18

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Daily Routine';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://cdn.shopify.com/s/files/1/1436/6710/articles/daily_activities_300x300.png?v=1472564886';
   image3 = '';
 
   constructor() { }
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'Aquaman', 'The Lion King'];
 
    constructor() { }
 
    ngOnInit() {
    }
-
-}
+   }
